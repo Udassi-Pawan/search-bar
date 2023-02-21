c373ec24fa490d8853fa4e33e28c1e25515c28e9
udassi <pawanudassi@hotmail.com>
2023-02-21 16:26:52 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelector("button").addEventListener("click", () => {
+  console.log("af");
+  document.querySelector("section").classList.toggle("show");
+  document.querySelector("input").focus();
+
+});
